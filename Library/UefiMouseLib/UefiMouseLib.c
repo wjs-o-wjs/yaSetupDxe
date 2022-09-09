3594e2341212fd7cccfd39a88b63a2a28951b728
sprayman1999 <196011564@qq.com>
2022-09-09 18:19:20 +0800

    Update: Add InitMouseSSP.

--- Library/UefiMouseLib/UefiMouseLib.c
+++ Library/UefiMouseLib/UefiMouseLib.c
@@ -1,10 +1,10 @@
 
 #include "UefiMouseLib.h"
 
-EFI_SIMPLE_POINTER_PROTOCOL * SPP;
-void InitMouseSPP(IN EFI_SYSTEM_TABLE *SystemTable){
-    EFI_GUID spp_guid = {0x31878c87, 0xb75, 0x11d5, \
-                               {0x9a, 0x4f, 0x0, 0x90,    \
-                                0x27, 0x3f, 0xc1, 0x4d}};
+EFI_SIMPLE_POINTER_PROTOCOL * SPP = NULL;
+EFI_STATUS EFIAPI InitMouseSPP(IN EFI_SYSTEM_TABLE *SystemTable)
+{
+    EFI_GUID spp_guid = {0x31878c87, 0xb75, 0x11d5, {0x9a, 0x4f, 0x0, 0x90, 0x27, 0x3f, 0xc1, 0x4d}};
     SystemTable->BootServices->LocateProtocol(&spp_guid,NULL,(void **)&SPP);
+    return (SPP != NULL)? EFI_SUCCESS : EFI_LOAD_ERROR;
 }
--- Library/UefiMouseLib/UefiMouseLib.h
+++ Library/UefiMouseLib/UefiMouseLib.h
@@ -5,7 +5,7 @@
 #include <Protocol/SimplePointer.h>
 
 
-void InitMouseSPP(IN EFI_SYSTEM_TABLE *SystemTable);
+EFI_STATUS EFIAPI InitMouseSPP(IN EFI_SYSTEM_TABLE *SystemTable);
 unsigned long long GetMouseState(IN EFI_SYSTEM_TABLE *SystemTable,OUT EFI_SIMPLE_POINTER_STATE * SimplePointerState);
 
 #endif
