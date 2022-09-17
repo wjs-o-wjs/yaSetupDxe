dafde9df71f90a0066cfee2ac820c45ceeec304e
fhh200000 <1041154549@qq.com>
2022-09-17 20:22:11 +0800

    Update: compiled FreeTpye(I don't know how to use it....)

---
+++ non-UEFI/concept-tests/3rdparty/freetype2/ftuefi.c
@@ -0,0 +1,7 @@
+FT_BASE_DEF( FT_Error )
+  FT_Stream_Open( FT_Stream    stream,
+                  const char*  filepathname )
+  {
+      //NO NEED TO OPEN FILE!
+      return 0;
+  }
--- non-UEFI/concept-tests/blend.c
+++ non-UEFI/concept-tests/blend.c
@@ -58,9 +58,9 @@ void alpha_blending_naive(unsigned* a, unsigned* b, unsigned* res)
 }
 uint32_t alpha_blending_pixel(uint32_t a, uint32_t b)
 {
-
+    return 0;
 }
 pixel* alpha_blending_bitmap(pixel* front,uint32_t front_width, uint32_t front_height, pixel* back, uint32_t back_width, uint32_t back_height, uint32_t offset_x, uint32_t offset_y)
 {
-
+    return 0;
 }
