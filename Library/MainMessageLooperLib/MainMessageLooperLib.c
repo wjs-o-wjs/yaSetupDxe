1bff6499da90537d5cac8827c94ff4dc633d307b
fhh200000 <1041154549@qq.com>
2022-12-11 22:38:03 +0800

    Update: Message Looper End event.

--- Library/MainMessageLooperLib/MainMessageLooperLib.c
+++ Library/MainMessageLooperLib/MainMessageLooperLib.c
@@ -1,7 +1,8 @@
 #include <Library/MainMessageLooperLib.h>
 #include <Library/BaseMemoryLib.h>
 
-extern EFI_SYSTEM_TABLE *gST;
+extern EFI_SYSTEM_TABLE   *gST;
+extern EFI_BOOT_SERVICES  *gBS;
 //So far, holding a cycle queue of 128 elements is REALLY enough.
 static MESSAGE_LOOPER_MESSAGE_STRUCTURE MessageQueue[_PCD_VALUE_MessageQueueSize];
 static volatile UINTN QueuePointerFront=0,QueuePointerBack=0;
@@ -32,6 +33,36 @@ MessageQueueDequeue
   QueuePointerFront = MessageQueueNext(QueuePointerFront);
 }
 
+//STATIC
+VOID
+EFIAPI
+MessageLooperEvent
+(
+  IN  EFI_EVENT                Event,
+  IN  VOID                     *Context
+)
+{
+  gST->ConOut->OutputString(gST->ConOut,L"Looper event triggered.\n");
+
+  (VOID)Event;
+  (VOID)Context;
+}
+
+STATIC EFI_EVENT MessageLooperEndEvent;
+//STATIC
+VOID
+EFIAPI
+MessageLooperEndEventNull
+(
+  IN  EFI_EVENT                Event,
+  IN  VOID                     *Context
+)
+{
+  (VOID)Event;
+  (VOID)Context;
+  gBS->SignalEvent(MessageLooperEndEvent);
+  return;
+}
 EFI_STATUS
 EFIAPI
 EnterMainMessageLoop
@@ -39,7 +70,20 @@ EnterMainMessageLoop
   VOID
 )
 {
-  gST->ConOut->OutputString(gST->ConOut,L"Entering Main Message Loop.\n");
-  return EFI_SUCCESS;
+  EFI_STATUS Status;
+  //UINTN Index;
+  gST->ConOut->OutputString(gST->ConOut,L"Entering Main Message Loop.\r\n");
+  Status = gBS->CreateEvent(0,TPL_APPLICATION,MessageLooperEndEventNull,NULL,&MessageLooperEndEvent);
+  if(EFI_ERROR(Status)) {
+    gST->ConOut->OutputString(gST->ConOut,L"Cannot create Looper End Event.\r\n");
+    return Status;
+  }
+  gST->ConOut->OutputString(gST->ConOut,L"Create Message loop done.\r\n");
+  Status = gBS->WaitForEvent(1,MessageLooperEndEvent,NULL);
+  if(EFI_ERROR(Status)) {
+    gST->ConOut->OutputString(gST->ConOut,L"Cannot start message looper Event.\r\n");
+    return Status;
+  }
+  return Status;
 }
 
